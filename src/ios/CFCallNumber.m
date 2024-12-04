c0d4ef21a9496c03f646be95e3efc478bb0bdb56
gelinger777 <gelinger777@users.noreply.github.com>
2024-12-04 14:46:01 +0100

    Update CFCallNumber.m
    if ios 18 make new code, else old one . just in case

--- src/ios/CFCallNumber.m
+++ src/ios/CFCallNumber.m
@@ -7,27 +7,54 @@
 
     [self.commandDelegate runInBackground:^{
 
-        __block CDVPluginResult* pluginResult = nil;
-        NSString* number = [command.arguments objectAtIndex:0];
-        number = [number stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+        if (@available(iOS 18.0, *)) {
+            // New code for iOS >= 18
+            __block CDVPluginResult* pluginResult = nil;
+            NSString* number = [command.arguments objectAtIndex:0];
+            number = [number stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
 
-        if( ! [number hasPrefix:@"tel:"]){
-            number =  [NSString stringWithFormat:@"tel:%@", number];
-        }
+            if( ! [number hasPrefix:@"tel:"]){
+                number =  [NSString stringWithFormat:@"tel:%@", number];
+            }
 
-        NSURL* telURL = [NSURL URLWithString:number];
+            NSURL* telURL = [NSURL URLWithString:number];
 
-        if (![[UIApplication sharedApplication] canOpenURL:telURL]) {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
+            if (![[UIApplication sharedApplication] canOpenURL:telURL]) {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
+            } else {
+                [[UIApplication sharedApplication] openURL:telURL options:@{} completionHandler:^(BOOL success) {
+                    if (success) {
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                    } else {
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
+                    }
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                }];
+            }
         } else {
-            [[UIApplication sharedApplication] openURL:telURL options:@{} completionHandler:^(BOOL success) {
-                if (success) {
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-                } else {
-                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
-                }
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-            }];
+            // Old code for iOS < 18
+            __block CDVPluginResult* pluginResult = nil;
+            NSString* number = [command.arguments objectAtIndex:0];
+            number = [number stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+
+            if( ! [number hasPrefix:@"tel:"]){
+                number =  [NSString stringWithFormat:@"tel:%@", number];
+            }
+
+            NSURL* telURL = [NSURL URLWithString:number];
+
+            if (![[UIApplication sharedApplication] canOpenURL:telURL]) {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"NoFeatureCallSupported"];
+            } else {
+                [[UIApplication sharedApplication] openURL:telURL options:@{} completionHandler:^(BOOL success) {
+                    if (success) {
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                    } else {
+                        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"CouldNotCallPhoneNumber"];
+                    }
+                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                }];
+            }
         }
     }];
 }
